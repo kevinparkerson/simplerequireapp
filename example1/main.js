c1d984ff64994c2141fe59588dba5ccf591c4f21
kevinparkerson <kparkerson@exacttarget.com>
2014-10-22 13:03:29 -0400

    initial example

---
+++ example1/main.js
@@ -0,0 +1,9 @@
+//A very basic module
+define(function(require){
+	var $ = require('jquery');
+	var _ = require('underscore');
+
+	console.log('Hello World!');
+	console.log('jQuery: ', $);
+	console.log('underscore: ', _);
+});
